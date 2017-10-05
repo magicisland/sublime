8dce6b2d010ee36f6dedf7bf940720d452c98df4
magicisland <marcelojavierrodriguez@gmail.com>
2017-10-05 08:32:47 -0300

    cambiado el nombre de la interface

---
+++ src/server/IService.java
@@ -0,0 +1,17 @@
+package server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/servicio")
+public interface Service {
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createProduct(Object object);
+	
+	
+}
