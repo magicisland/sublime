53b893e08d60fe85104f6b1abaa15a2ef5f04802
magicisland <marcelojavierrodriguez@gmail.com>
2017-10-05 20:00:37 -0300

    GET returning raw POJO

--- src/server/Service.java
+++ src/server/Service.java
@@ -3,14 +3,28 @@ package server;
 
 import java.net.URI;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
-public class Service implements IService{
+import com.google.gson.Gson;
 
-	@Override
+import model.Pojo;
+
+@Path("/servicio")
+
+public class Service {
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+
 	public Response createProduct(Object object) {
 		// TODO Auto-generated method stub
 		
@@ -22,7 +36,7 @@ public class Service implements IService{
 		  In counterpart, we return 200 code; it means .. everything is OK 
 		*/
 		
-		String body = "";
+		String body = " POST ";
 		int productId = 0;
 		
 		ResponseBuilder responseBuilder = Response.created(URI.create("/products/"+productId));
@@ -33,6 +47,20 @@ public class Service implements IService{
 		
 		return response;
 	
+	}
+
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Pojo getProduct(@PathParam("id")int id) {
+		// TODO Auto-generated method stub
+		
+		Pojo pojo = new Pojo();
+		pojo.setId(1);
+		pojo.setName("Marcelo");
+		return pojo;
+		
+			
 	}
 
 }
